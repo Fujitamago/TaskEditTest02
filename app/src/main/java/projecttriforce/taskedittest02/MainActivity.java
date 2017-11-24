cdbd2d58056d98b7c3388c66a83b9a31925cede6
enma <takanamoo@gmail.com>
2017-11-24 15:04:23 +0900

    ◎ Stetho-Realmの導入 ◎ First Commit

---
+++ app/src/main/java/projecttriforce/taskedittest02/MainActivity.java
@@ -0,0 +1,49 @@
+package projecttriforce.taskedittest02;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import io.realm.Realm;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Realm.init(this);
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
+
+        setContentView(R.layout.activity_main);
+
+
+    }
+    //着手日をタップした時
+    public void DateTapped(View view) {
+        Intent intent = new Intent(this, projecttriforce.taskedittest02.DatePickerT.class);
+        //完全修飾名でないとできないかも
+        startActivity(intent);
+        //アクティビティを起動
+    }
+
+    public void Get(View view){
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("DateOK");
+        TextView txt = (TextView)findViewById(R.id.StarDate02);
+        TextView txt1 = (TextView)findViewById(R.id.DeadDate);
+        txt.setText(str);
+        txt1.setText(str);
+    }
+
+
+}
