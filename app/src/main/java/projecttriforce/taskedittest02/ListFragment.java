448b96913beffe52b9a89c21278ffc66f96b4984
enma <takanamoo@gmail.com>
2017-11-26 10:43:36 +0900

    commit

---
+++ app/src/main/java/projecttriforce/taskedittest02/ListFragment.java
@@ -0,0 +1,10 @@
+package projecttriforce.taskedittest02;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by takan on 2017/11/25.
+ */
+
+public class ListFragment extends FragmentActivity {
+}
